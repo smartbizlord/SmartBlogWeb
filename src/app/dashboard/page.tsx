60537ef830091949ba5ec27f8ae5704d6781d4da
unknown <smartbizlord@gmail.com>
2023-08-25 15:36:33 +0100

    initial commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import styles from './page.module.css'
+
+// className={styles.}
+
+const Dashboard = () => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
